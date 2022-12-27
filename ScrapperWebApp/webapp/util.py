811805e5fa8553cbb43a64bd117fc06ab770700e
Ilerioluwa Albert Akinola <ilerioluwaakinola@gmail.com>
2022-12-27 16:35:38 +0200

    Added functions for logic of the app

--- ScrapperWebApp/webapp/util.py
+++ ScrapperWebApp/webapp/util.py
@@ -3,6 +3,51 @@ import json
 import random
 import time
 
+# Search for item
+def searchfunction(item):
+    searchUrl = f"https://steamcommunity.com/market/search/render/?query={item}&start=0" \
+                f"&count=10&search_descriptions=0&sort_column=default&sort_dir=desc&norender=1"
+
+    r = requests.get(searchUrl) # Get page
+    return r
 
 
+# turn content to a json format
+def makeJsonfile(pageContent):
+    
+    jsonfile = pageContent.content
+    jsonfile = json.loads(jsonfile)
+    
+    return jsonfile
 
+
+# Formating results to return a dictiuonary
+def formattedResult(item):
+
+    resultDictionary = {}
+    # Get total number of items 
+
+    gettotalItems = searchfunction(item)
+    gettotalItems = makeJsonfile(totalItems)
+    totalItems = gettotalItems['total_count']
+
+    for currPos in range(0, totalItems + 50, 50):
+        time.sleep(random.uniform(0.5, 2.5))
+
+        itemsUrl = f"https://steamcommunity.com/market/search/render/?query={item}&start={currPos}" \
+                   f"&count=100&search_descriptions=0&sort_column=default&sort_dir=desc&norender=1&count=5000"
+        
+        getAllItems = requests.get(itemsUrl)
+
+        allItems = makeJsonfile(getAllItems)
+        allItems = allItems['results']
+
+        for currItem in allItems:
+            if currItem['asset_description']['tradable']:
+                itemName = currItem['name']
+                resultDictionary[itemName] = {
+                    "Game Name": currItem['app_name'],
+                    "Game ID": currItem['asset_description']['appid'],
+                    "Sell Price": currItem['sell_price_text']
+                }
+    return resultDictionary
--- ScrapperWebApp/webapp/views.py
+++ ScrapperWebApp/webapp/views.py
@@ -4,3 +4,6 @@ from . import util
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def searchResult(request):
+    return render(request, "results.html")
